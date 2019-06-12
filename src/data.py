56e02040ea1f5ef815610e1fdd13721b0ec4d176
ddddwy <duwanyu0706@163.com>
2019-06-12 13:11:40 -0400

    upload lm code

--- src/data.py
+++ src/data.py
@@ -1,184 +1,195 @@
-#Most of this file is copied form https://github.com/abisee/pointer-generator/blob/master/data.py
+import os
+import torch
+import dill
+import gzip
 
-import glob
-import random
-import struct
-import csv
-from tensorflow.core.example import example_pb2
+class convertvocab(object):
+    def __init__(self, load_from, save_to):
+        self.dictionary = Dictionary()
+        self.loadme = self.load_dict(load_from)
+        self.save_to = self.save_dict(save_to)
 
-# <s> and </s> are used in the data files to segment the abstracts into sentences. They don't receive vocab ids.
-SENTENCE_START = '<s>'
-SENTENCE_END = '</s>'
+    def save_dict(self, path):
+        with open(path, 'wb') as f:
+            torch.save(self.dictionary, f, pickle_module=dill)
 
-PAD_TOKEN = '[PAD]' # This has a vocab id, which is used to pad the encoder input, decoder input and target sequence
-UNKNOWN_TOKEN = '[UNK]' # This has a vocab id, which is used to represent out-of-vocabulary words
-START_DECODING = '[START]' # This has a vocab id, which is used at the start of every decoder input sequence
-STOP_DECODING = '[STOP]' # This has a vocab id, which is used at the end of untruncated target sequences
+    def load_dict(self, path):
+        assert os.path.exists(path)
+        with open(path, 'r') as f:
+            for line in f:
+                self.dictionary.add_word(line.strip())
 
-# Note: none of <s>, </s>, [PAD], [UNK], [START], [STOP] should appear in the vocab file.
+class Dictionary(object):
+    def __init__(self):
+        self.word2idx = {}
+        self.tag2idx = {}
+        self.idx2tag = []
+        self.idx2word = []
 
+    def add_word(self, word):
+        if word not in self.word2idx:
+            self.idx2word.append(word)
+            self.word2idx[word] = len(self.idx2word) - 1
+        return self.word2idx[word]
 
-class Vocab(object):
+    def add_tag(self, tag):
+        if tag not in self.tag2idx:
+            self.idx2tag.append(tag)
+            self.tag2idx[tag] = len(self.idx2tag) - 1
+        return self.tag2idx[tag]
 
-  def __init__(self, vocab_file, max_size):
-    self._word_to_id = {}
-    self._id_to_word = {}
-    self._count = 0 # keeps track of total number of words in the Vocab
+    def __len__(self):
+        return len(self.idx2word)
 
-    # [PAD], [UNK], [START] and [STOP] get the ids 0,1,2,3.
-    for w in [PAD_TOKEN, UNKNOWN_TOKEN, START_DECODING, STOP_DECODING]:
-      self._word_to_id[w] = self._count
-      self._id_to_word[self._count] = w
-      self._count += 1
-
-    # Read the vocab file and add words up to max_size
-    with open(vocab_file, 'r', encoding='utf8') as vocab_f:
-      for line in vocab_f:
-        pieces = line.split()
-        if len(pieces) != 2:
-          print('Warning: incorrectly formatted line in vocabulary file: %s\n' % line)
-          continue
-        w = pieces[0]
-        if w in [SENTENCE_START, SENTENCE_END, UNKNOWN_TOKEN, PAD_TOKEN, START_DECODING, STOP_DECODING]:
-          raise Exception('<s>, </s>, [UNK], [PAD], [START] and [STOP] shouldn\'t be in the vocab file, but %s is' % w)
-        if w in self._word_to_id:
-          raise Exception('Duplicated word in vocabulary file: %s' % w)
-        self._word_to_id[w] = self._count
-        self._id_to_word[self._count] = w
-        self._count += 1
-        if max_size != 0 and self._count >= max_size:
-          #print("max_size of vocab was specified as %i; we now have %i words. Stopping reading." % (max_size, self._count))
-          break
-
-    #print("Finished constructing vocabulary of %i total words. Last word added: %s" % (self._count, self._id_to_word[self._count-1]))
-
-  def word2id(self, word):
-    if word not in self._word_to_id:
-      return self._word_to_id[UNKNOWN_TOKEN]
-    return self._word_to_id[word]
-
-  def id2word(self, word_id):
-    if word_id not in self._id_to_word:
-      raise ValueError('Id not found in vocab: %d' % word_id)
-    return self._id_to_word[word_id]
-
-  def size(self):
-    return self._count
-
-  def write_metadata(self, fpath):
-    print("Writing word embedding metadata file to %s..." % (fpath))
-    with open(fpath, "w") as f:
-      fieldnames = ['word']
-      writer = csv.DictWriter(f, delimiter="\t", fieldnames=fieldnames)
-      for i in range(self.size()):
-        writer.writerow({"word": self._id_to_word[i]})
-
-
-def example_generator(data_path, single_pass):
-  while True:
-    filelist = glob.glob(data_path) # get the list of datafiles
-    assert filelist, ('Error: Empty filelist at %s' % data_path) # check filelist isn't empty
-    if single_pass:
-      filelist = sorted(filelist)
-    else:
-      random.shuffle(filelist)
-    for f in filelist:
-      reader = open(f, 'rb')
-      while True:
-        len_bytes = reader.read(8)
-        if not len_bytes: break # finished reading this file
-        str_len = struct.unpack('q', len_bytes)[0]
-        example_str = struct.unpack('%ds' % str_len, reader.read(str_len))[0]
-        yield example_pb2.Example.FromString(example_str)
-    if single_pass:
-      #print("example_generator completed reading all datafiles. No more data.")
-      break
-
-
-def article2ids(article_words, vocab):
-  ids = []
-  oovs = []
-  unk_id = vocab.word2id(UNKNOWN_TOKEN)
-  for w in article_words:
-    i = vocab.word2id(w)
-    if i == unk_id: # If w is OOV
-      if w not in oovs: # Add to list of OOVs
-        oovs.append(w)
-      oov_num = oovs.index(w) # This is 0 for the first article OOV, 1 for the second article OOV...
-      ids.append(vocab.size() + oov_num) # This is e.g. 50000 for the first article OOV, 50001 for the second...
-    else:
-      ids.append(i)
-  return ids, oovs
-
-
-def abstract2ids(abstract_words, vocab, article_oovs):
-  ids = []
-  unk_id = vocab.word2id(UNKNOWN_TOKEN)
-  for w in abstract_words:
-    i = vocab.word2id(w)
-    if i == unk_id: # If w is an OOV word
-      if w in article_oovs: # If w is an in-article OOV
-        vocab_idx = vocab.size() + article_oovs.index(w) # Map to its temporary article OOV number
-        ids.append(vocab_idx)
-      else: # If w is an out-of-article OOV
-        ids.append(unk_id) # Map to the UNK token id
-    else:
-      ids.append(i)
-  return ids
-
-
-def outputids2words(id_list, vocab, article_oovs):
-  words = []
-  for i in id_list:
-    try:
-      w = vocab.id2word(i) # might be [UNK]
-    except ValueError as e: # w is OOV
-      assert article_oovs is not None, "Error: model produced a word ID that isn't in the vocabulary. This should not happen in baseline (no pointer-generator) mode"
-      article_oov_idx = i - vocab.size()
-      try:
-        w = article_oovs[article_oov_idx]
-      except ValueError as e: # i doesn't correspond to an article oov
-        raise ValueError('Error: model produced word ID %i which corresponds to article OOV %i but this example only has %i article OOVs' % (i, article_oov_idx, len(article_oovs)))
-    words.append(w)
-  return words
-
-
-def abstract2sents(abstract):
-  cur = 0
-  sents = []
-  abstract = str(abstract, encoding='utf8')
-  while True:
-    try:
-      start_p = abstract.index(SENTENCE_START, cur)
-      end_p = abstract.index(SENTENCE_END, start_p + 1)
-      cur = end_p + len(SENTENCE_END)
-      sents.append(abstract[start_p+len(SENTENCE_START):end_p])
-    except ValueError as e: # no more sentences
-      return sents
-
-
-def show_art_oovs(article, vocab):
-  unk_token = vocab.word2id(UNKNOWN_TOKEN)
-  words = article.split(' ')
-  words = [("__%s__" % w) if vocab.word2id(w)==unk_token else w for w in words]
-  out_str = ' '.join(words)
-  return out_str
-
-
-def show_abs_oovs(abstract, vocab, article_oovs):
-  unk_token = vocab.word2id(UNKNOWN_TOKEN)
-  words = abstract.split(' ')
-  new_words = []
-  for w in words:
-    if vocab.word2id(w) == unk_token: # w is oov
-      if article_oovs is None: # baseline mode
-        new_words.append("__%s__" % w)
-      else: # pointer-generator mode
-        if w in article_oovs:
-          new_words.append("__%s__" % w)
+class SentenceCorpus(object):
+    def __init__(self, seq_len, lm_path, tag_path, save_to='lm_data.bin', testflag=False,
+                 trainfname, validfname, testfname):
+        self.seq_len = seq_len
+        if not testflag:
+            self.dictionary = Dictionary()
+            self.train_lm = self.tokenize(os.path.join(lm_path, trainfname))
+            self.valid_lm = self.tokenize_with_unks(os.path.join(lm_path, validfname))
+            self.train_tag = self.tokenize_tag(os.path.join(tag_path, trainfname))
+            self.valid_tag = self.tokenize_tag_with_unks(os.path.join(tag_path, validfname))
+            self.save_to = self.save_dict(save_to)
         else:
-          new_words.append("!!__%s__!!" % w)
-    else: # w is in-vocab word
-      new_words.append(w)
-  out_str = ' '.join(new_words)
-  return out_str
+            self.dictionary = self.load_dict(save_to)
+            self.test_lm = self.sent_tokenize_with_unks(os.path.join(lm_path, testfname))
+            self.test_tag = self.sent_tokenize_tag_with_unks(os.path.join(tag_path, testfname))
+
+    def save_dict(self, path):
+        with open(path, 'wb') as f:
+            torch.save(self.dictionary, f, pickle_module=dill)
+
+    def load_dict(self, path):
+        assert os.path.exists(path)
+        with open(path, 'rb') as f:
+            fdata = torch.load(f, pickle_module=dill)
+            if type(fdata) == type(()):
+                # compatibility with old pytorch LM saving
+                return(fdata[3])
+            return(fdata)
+
+    def tokenize_tag(self, path):
+        """Tokenizes and gets POS tags for a text file."""
+        assert os.path.exists(path)
+
+        # Add words and tags to the dictionary
+        with open(path, 'r') as f:
+            lines = f.read().split('\n')
+        for line in lines:
+            if line.strip() != "":
+                tags = line.strip().split()
+                for tag in tags:
+                    self.dictionary.add_tag(tag)
+                    
+        # Tokenize file content
+        tag_ids = torch.zeros((len(lines), self.seq_len), dtype=torch.long)
+        for i, line in enumerate(lines):
+            if line.strip() != "":
+                tags = line.strip().split()
+                for j, tag in enumerate(tags):
+                    tag_ids[i, j] = self.dictionary.tag2idx[tag]
+        return tag_ids
+
+    def tokenize_tag_with_unks(self, path):
+        """Tokenizes and gets POS tags for a text file."""
+        assert os.path.exists(path)
+        with open(path, 'r') as f:
+            lines = f.read().split('\n')
+        # Tokenize file content
+        tag_ids = torch.zeros((len(lines), self.seq_len), dtype=torch.long)
+        for i, line in enumerate(lines):
+            if line.strip() != "":
+                tags = line.strip().split()
+                for j, tag in enumerate(tags):
+                    if tag not in self.dictionary.tag2idx:
+                        tag_ids[i, j] = self.dictionary.add_tag["<UNK>"]
+                    else:
+                        tag_ids[i, j] = self.dictionary.tag2idx[tag]
+        return tag_ids
+    
+    def sent_tokenize_tag_with_unks(self, path):
+        """Tokenizes and gets POS tags for a text file."""
+        assert os.path.exists(path)
+        with open(path, 'r') as f:
+            lines = f.read().split('\n')
+        # Tokenize file content
+        all_tags = []
+        for i, line in enumerate(lines):
+            if line.strip() != "":
+                tags = line.strip().split()
+                tag_ids = torch.LongTensor(len(tags))
+                for j, tag in enumerate(tags):
+                    if tag not in self.dictionary.tag2idx:
+                        tag_ids[i, j] = self.dictionary.add_tag["<UNK>"]
+                    else:
+                        tag_ids[i, j] = self.dictionary.tag2idx[tag]
+                all_tags.append(tag_ids)
+        return all_tags
+
+    def tokenize(self, path):
+        """Tokenizes a text file."""
+        assert os.path.exists(path)
+        # Add words to the dictionary
+        with open(path, 'r+', encoding="utf-8") as f:
+            lines = f.read().split('\n')
+        for line in lines:
+            if line.strip() == "":
+                continue
+            words = line.strip().split()
+            for word in words:
+                self.dictionary.add_word(word)
+
+        # Tokenize file content
+        with open(path, 'r+', encoding="utf-8") as f:
+            lines = f.read().split('\n')
+        ids = torch.zeros((len(lines), self.seq_len), dtype=torch.long)
+        for i, line in enumerate(lines):
+            if line.strip() == "":
+                continue
+            words = line.strip().split()
+            for j, word in enumerate(words):
+                ids[i, j] = self.dictionary.word2idx[word]
+        return ids
+
+    def tokenize_with_unks(self, path):
+        """Tokenizes a text file, adding unks if needed."""
+        assert os.path.exists(path)
+        # Tokenize file content
+        with open(path, 'r+', encoding="utf-8") as f:
+            lines = f.read().split('\n')
+        ids = torch.zeros((len(lines), self.seq_len), dtype=torch.long)
+        for i, line in enumerate(lines):
+            if line.strip() == "":
+                continue
+            words = line.strip().split()
+            for j, word in enumerate(words):
+                if word not in self.dictionary.word2idx:
+                    ids[i, j] = self.dictionary.add_word("<unk>")
+                else:
+                    ids[i, j] = self.dictionary.word2idx[word]
+        return ids
+
+    def sent_tokenize_with_unks(self, path):
+        """Tokenizes a text file into sentences, adding unks if needed."""
+        assert os.path.exists(path)
+        with open(path, 'r+', encoding="utf-8") as f:
+            lines = f.read().split('\n')
+        all_ids = []
+        sents = []
+        for line in lines:
+            if line.strip() == "":
+                continue
+            sents.append(line.strip())
+            words = line.strip().split()
+            # tokenize file content
+            ids = torch.LongTensor(len(words))
+            for j, word in enumerate(words):
+                if word not in self.dictionary.word2idx:
+                    ids[j] = self.dictionary.add_word("<unk>")
+                else:
+                    ids[j] = self.dictionary.word2idx[word]
+                token += 1
+            all_ids.append(ids)                
+        return (sents, all_ids)
--- src/model.py
+++ src/model.py
@@ -1,202 +1,44 @@
-from __future__ import unicode_literals, print_function, division
-
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from numpy import random
+from torch.autograd import Variable
 
-import config
-from cfsm import CFSM
+class RNNModel(nn.Module):
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5):
+        super(RNNModel, self).__init__()
+        self.drop = nn.Dropout(dropout)
+        self.emb = nn.Embedding(ntoken, ninp)
+        if rnn_type in ['LSTM', 'GRU']:
+            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout=dropout)
+        else:
+            try:
+                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
+            except KeyError:
+                raise ValueError( """An invalid option for `--model` was supplied,
+                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=dropout)
+        self.decode = nn.Linear(nhid, ntoken)
 
-use_cuda = config.use_gpu and torch.cuda.is_available()
+        self.init_weights()
+        self.rnn_type = rnn_type
+        self.nhid = nhid
+        self.nlayers = nlayers
 
-random.seed(123)
-torch.manual_seed(123)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(123)
+    def init_weights(self):
+        initrange = 0.1
+        self.emb.weight.data.uniform_(-initrange, initrange)
+        self.decode.bias.data.fill_(0)
+        self.decode.weight.data.uniform_(-initrange, initrange)
 
-def init_lstm_wt(lstm):
-    for names in lstm._all_weights:
-        for name in names:
-            if name.startswith('weight_'):
-                wt = getattr(lstm, name)
-                wt.data.uniform_(-config.rand_unif_init_mag, config.rand_unif_init_mag)
-            elif name.startswith('bias_'):
-                # set forget bias to 1
-                bias = getattr(lstm, name)
-                n = bias.size(0)
-                start, end = n // 4, n // 2
-                bias.data.fill_(0.)
-                bias.data[start:end].fill_(1.)
+    def forward(self, input, hidden):
+        word_emb = self.drop(self.emb(input))  # [seq_len, batch_size, emb_size]
+        output, hidden = self.rnn(word_emb, hidden)  # hidden = [num_layers, batch, hidden_size]
+        output = self.drop(output)   # [seq_len, batch_size, hidden_size]
+        decoded = self.decode(output.view(output.size(0)*output.size(1), output.size(2))) # [seq_len*batch_size, ntoken]
+        return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
 
-def init_linear_wt(linear):
-    linear.weight.data.normal_(std=config.trunc_norm_init_std)
-    if linear.bias is not None:
-        linear.bias.data.normal_(std=config.trunc_norm_init_std)
-
-def init_wt_normal(wt):
-    wt.data.normal_(std=config.trunc_norm_init_std)
-
-def init_wt_unif(wt):
-    wt.data.uniform_(-config.rand_unif_init_mag, config.rand_unif_init_mag)
-
-
-class Encoder(nn.Module):
-    def __init__(self):
-        super(Encoder, self).__init__()
-        # shape = (batch_size, max_seq_len, emb_dim)
-        self.embedding = nn.Embedding(config.vocab_size, config.emb_dim)
-        init_wt_normal(self.embedding.weight)
-        
-        # shape = (batch_size, seq_len, 2*hidden_size)
-        self.lstm = nn.LSTM(config.emb_dim, config.hidden_dim, num_layers=1, 
-                            batch_first=True, bidirectional=True)
-        init_lstm_wt(self.lstm)
-        
-        self.W_h = nn.Linear(config.hidden_dim * 2, config.hidden_dim * 2, bias=False)
-
-    #seq_lens should be in descending order
-    def forward(self, input, seq_lens):
-        embedded = self.embedding(input)    # shape = (batch_size, max_seq_len, emb_dim)
-
-        packed = pack_padded_sequence(embedded, seq_lens, batch_first=True)
-        output, hidden = self.lstm(packed)  # hidden shape = ((batch, 2, hidden_size), (batch, 2, hidden_size))
-
-        # shape = (batch_size, max_seq_len, 2*hidden_size)
-        encoder_outputs, _ = pad_packed_sequence(output, batch_first=True)  # h dim = B x t_k x n
-        encoder_outputs = encoder_outputs.contiguous()
-        
-        # shape = (batch_size*max_seq_len, 2*hidden_size)
-        encoder_feature = encoder_outputs.view(-1, 2*config.hidden_dim)  # B * t_k x 2*hidden_dim
-        encoder_feature = self.W_h(encoder_feature) # shape = (batch_size*max_seq_len, 2*hidden_size)
-
-        return encoder_outputs, encoder_feature, hidden
-    
-
-class ReduceState(nn.Module):
-    def __init__(self):
-        super(ReduceState, self).__init__()
-
-        self.reduce_h = nn.Linear(config.hidden_dim * 2, config.hidden_dim)
-        init_linear_wt(self.reduce_h)
-        self.reduce_c = nn.Linear(config.hidden_dim * 2, config.hidden_dim)
-        init_linear_wt(self.reduce_c)
-
-    def forward(self, hidden):
-        h, c = hidden # h, c dim = 2 x b x hidden_dim
-        h_in = h.transpose(0, 1).contiguous().view(-1, config.hidden_dim * 2)   # shape = (batch_size, 2*hidden_size)
-        hidden_reduced_h = F.relu(self.reduce_h(h_in))  # shape = (batch_size, hidden_size)
-        c_in = c.transpose(0, 1).contiguous().view(-1, config.hidden_dim * 2)
-        hidden_reduced_c = F.relu(self.reduce_c(c_in))  # shape = (batch_size, hidden_size)
-
-        return (hidden_reduced_h.unsqueeze(0), hidden_reduced_c.unsqueeze(0)) # h, c dim = 1 x b x hidden_dim
-
-
-class Attention(nn.Module):
-    def __init__(self):
-        super(Attention, self).__init__()
-        # attention
-        self.decode_proj = nn.Linear(config.hidden_dim * 2, config.hidden_dim * 2)
-        self.v = nn.Linear(config.hidden_dim * 2, 1, bias=False)
-
-    def forward(self, s_t_hat, encoder_outputs, encoder_feature, enc_padding_mask):
-        b, t_k, n = list(encoder_outputs.size()) # b = batch_size, t_k = max_seq_len, n = 2*hidden_size
-
-        dec_fea = self.decode_proj(s_t_hat) # B x 2*hidden_dim
-        dec_fea_expanded = dec_fea.unsqueeze(1).expand(b, t_k, n).contiguous() # B x t_k x 2*hidden_dim
-        dec_fea_expanded = dec_fea_expanded.view(-1, n)  # B * t_k x 2*hidden_dim
-
-        att_features = encoder_feature + dec_fea_expanded # B * t_k x 2*hidden_dim
-
-        e = torch.tanh(att_features) # B * t_k x 2*hidden_dim
-        scores = self.v(e)  # B * t_k x 1
-        scores = scores.view(-1, t_k)  # B x t_k
-        
-        # NOTE: Here may lead to NAN problem!!!
-        attn_dist_ = F.softmax(scores, dim=1) * enc_padding_mask # B x t_k
-        normalization_factor = attn_dist_.sum(1, keepdim=True)
-        attn_dist = attn_dist_ / normalization_factor
-
-        attn_dist = attn_dist.unsqueeze(1)  # B x 1 x t_k
-        c_t = torch.bmm(attn_dist, encoder_outputs)  # B x 1 x n
-        c_t = c_t.view(-1, config.hidden_dim * 2)  # B x 2*hidden_dim
-
-        attn_dist = attn_dist.view(-1, t_k)  # B x t_k
-
-        return c_t, attn_dist
-        
-
-class Decoder(nn.Module):
-    def __init__(self):
-        super(Decoder, self).__init__()
-        self.attention_network = Attention()
-        self.embedding = nn.Embedding(config.vocab_size, config.emb_dim)
-        init_wt_normal(self.embedding.weight)
-
-        self.x_context = nn.Linear(config.hidden_dim * 2 + config.emb_dim, config.emb_dim)
-
-        self.lstm = nn.LSTM(config.emb_dim, config.hidden_dim, num_layers=1, 
-                            batch_first=True, bidirectional=False)
-        init_lstm_wt(self.lstm)
-
-        # p_vocab
-        self.out1 = nn.Linear(config.hidden_dim * 3, config.hidden_dim)
-        # cfsm
-        self.cfsm = CFSM()
-
-    def forward(self, y_t_1, s_t_1, encoder_outputs, encoder_feature, enc_padding_mask, 
-                c_t_1, target_cluster, step):
-
-        if not self.training and step == 0:
-            h_decoder, c_decoder = s_t_1
-            s_t_hat = torch.cat((h_decoder.view(-1, config.hidden_dim),
-                                 c_decoder.view(-1, config.hidden_dim)), 1)  # B x 2*hidden_dim
-            c_t, _, _ = self.attention_network(s_t_hat, encoder_outputs, 
-                                               encoder_feature, enc_padding_mask)
-        
-        y_t_1_embd = self.embedding(y_t_1) #  B x emb_dim
-        x = self.x_context(torch.cat((c_t_1, y_t_1_embd), 1)) # B x emb_dim
-        lstm_out, s_t = self.lstm(x.unsqueeze(1), s_t_1)  # lstm_out = B x 1 x hidden_size, s_t = B x 1 x hidden_size
-
-        h_decoder, c_decoder = s_t
-        s_t_hat = torch.cat((h_decoder.view(-1, config.hidden_dim),
-                             c_decoder.view(-1, config.hidden_dim)), 1)  # B x 2*hidden_dim
-        c_t, attn_dist = self.attention_network(s_t_hat, encoder_outputs, 
-                                                encoder_feature, enc_padding_mask)
-
-        output = torch.cat((lstm_out.view(-1, config.hidden_dim), c_t), 1) # B x hidden_dim * 3
-        h_p = self.out1(output) # B x hidden_dim
-
-        final_cluster_dist, final_word_dist = self.cfsm(h_p, target_cluster)
-        
-        return final_cluster_dist, final_word_dist, s_t, c_t
-    
-
-class Model(object):
-    def __init__(self, model_file_path=None, is_eval=False):
-        encoder = Encoder()
-        decoder = Decoder()
-        reduce_state = ReduceState()
-
-        # shared the embedding between encoder and decoder
-        decoder.embedding.weight = encoder.embedding.weight
-        if is_eval:
-            encoder = encoder.eval()
-            decoder = decoder.eval()
-            reduce_state = reduce_state.eval()
-
-        if use_cuda:
-            encoder = encoder.cuda()
-            decoder = decoder.cuda()
-            reduce_state = reduce_state.cuda()
-
-        self.encoder = encoder.train()
-        self.decoder = decoder.train()
-        self.reduce_state = reduce_state.train()
-
-        if model_file_path is not None:
-            state = torch.load(model_file_path, map_location= lambda storage, location: storage)
-            self.encoder.load_state_dict(state['encoder_state_dict'])
-            self.decoder.load_state_dict(state['decoder_state_dict'], strict=False)
-            self.reduce_state.load_state_dict(state['reduce_state_dict'])
+    def init_hidden(self, bsz):
+        weight = next(self.parameters()).data
+        if self.rnn_type == 'LSTM':
+            return (Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()),
+                    Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()))
+        else:
+            return Variable(weight.new(self.nlayers, bsz, self.nhid).zero_())

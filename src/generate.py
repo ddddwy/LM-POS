938d01615ec5822b156dde309e5f98ad463c98e1
ddddwy <duwanyu0706@163.com>
2019-06-17 15:06:46 -0400

    fix bugs

--- src/generate.py
+++ src/generate.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import torch
+import torch.nn.functional as F
 from torch.autograd import Variable
 from main import build_dictionary
 
@@ -13,11 +14,13 @@ parser.add_argument('--tag_path', type=str, default='../data/tag.txt',
                     help='location of the CCG corpus')
 parser.add_argument('--seq_len', type=int, default=35,
                     help='max seqence length')
+parser.add_argument('--rnn_num', type=int, default=1,
+                    help='number of recurrent net')
 parser.add_argument('--checkpoint', type=str, default='../models/model.pt',
                     help='model checkpoint to use')
 parser.add_argument('--outf', type=str, default='../results/generated.txt',
                     help='output file for generated text')
-parser.add_argument('--words', type=int, default=10000,
+parser.add_argument('--words', type=int, default=1000,
                     help='number of words to generate')
 parser.add_argument('--seed', type=int, default=1234,
                     help='random seed')
@@ -48,7 +51,10 @@ word2idx, tag2idx, idx2word, idx2tag = build_dictionary(args.vocab_path, args.ta
 
 # Load model from checkpoint
 with open(args.checkpoint, 'rb') as f:
-    model = torch.load(f)
+    if args.cuda:
+        model = torch.load(f)
+    else:
+        model = torch.load(f, map_location='cpu')
 
 model.eval()
 if args.cuda:
@@ -58,7 +64,8 @@ else:
 
 with torch.no_grad():
     # Initialize model inputs
-    hidden = model.init_hidden(1)
+    hidden_word = model.init_hidden(1)
+    hidden_tag = model.init_hidden(1)
     input_token = Variable(3*torch.ones(1, 1).long())
     input_tag = Variable(3*torch.ones(1, 1).long())
     if args.cuda:
@@ -66,16 +73,37 @@ with torch.no_grad():
         input_tag = input_tag.cuda()
     
     with open(args.outf, 'w') as outf:
+        true_words = 0
         for i in range(args.words):
-            p_word, p_tag, hidden = model(input_token, input_tag, hidden)
-            word_idx = torch.multinomial(p_word.squeeze(), 1)
-            tag_idx = torch.multinomial(p_tag.squeeze(), 1)
-            input_token.data.fill_(word_idx)
-            word = idx2word[word_idx]
-            input_tag.data.fill_(tag_idx)
-            tag = idx2word[tag_idx]
-    
-            outf.write(word + ('\n' if i % 20 == 19 else ' '))
+            if args.rnn_num == 1:
+                # p_word = [1, ntoken]
+                p_word, p_tag, hidden_word = model(input_token, input_tag, hidden_word)
+            else:
+                p_word, p_tag, hidden_word, hidden_tag = model(input_token, input_tag, hidden_word, hidden_tag)
+            word_dist = F.softmax(p_word.squeeze(), dim=0)
+            word_idx = torch.multinomial(word_dist, 2, replacement=False)
+            if word_idx[0] == 0:
+                wid = word_idx[1]
+            else:
+                wid = word_idx[0]
+            tag_dist = F.softmax(p_tag.squeeze(), dim=0)
+            tag_idx = torch.multinomial(tag_dist, 2, replacement=False)
+            if tag_idx[0] == 0:
+                tid = tag_idx[1]
+            else:
+                tid = tag_idx[0]
+            input_token.data.fill_(wid)
+            word = idx2word[wid]
+            input_tag.data.fill_(tid)
+            tag = idx2word[tid]
+            
+            if word == '<eos>':
+                continue
+            else:
+                outf.write(word + ' ')
+                true_words += 1
+                if true_words % 20 == 19:
+                    outf.write('\n')
     
             if i % args.log_interval == 0:
                 print('| Generated {}/{} words'.format(i, args.words))
